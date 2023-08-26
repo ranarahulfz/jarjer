619e000845783b52330563c44503ebda1f7f841b
ranarahulfz <ranarahulfz@gmail.com>
2023-08-26 15:19:40 +0530

    adding README.md file.

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "Its an H1 tag."),
+    React.createElement("h2", {}, "Its an H2 tag."),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Its an H1 tag."),
+    React.createElement("h2", {}, "Its an H2 tag."),
+  ]),
+]);
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);

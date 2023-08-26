ad9b4444c3d123bcf33b0911442080b5ee7f8231
ranarahulfz <ranarahulfz@gmail.com>
2023-08-26 18:18:07 +0530

    optimizing the parcel config.

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Its an H1 tag."),
-    React.createElement("h2", {}, "Its an H2 tag."),
+    React.createElement("h1", { id: "title1" }, "This is Namaste React 🚀."),
+    React.createElement("h2", { id: "subTitle1" }, "By Rahul Rana"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Its an H1 tag."),
-    React.createElement("h2", {}, "Its an H2 tag."),
+    React.createElement("h1", { id: "title2" }, "Its an H1 tag."),
+    React.createElement("h2", { id: "subTitle2" }, "Its an H2 tag."),
   ]),
 ]);
-console.log(heading);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React 🚀
+
+# Parcel (Bundler):
+
+- Creating Dev Builds
+- Creating Local Server and Keep App Up and Running
+- HMR = Hot Module Replacement
+- File wathcing algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing code
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - To support older browsers
+- Diagnostic - Better Error Handling
+- HTTPS - Parcel give you access to SSL certificate
+- Tree Shaking - Remove unused code
+- Different DEV and PROD bundles
